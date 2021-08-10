dbad4cd21493c487d9c58768b5bd9aff21fb4cab
Daniel Sehn Colao <66954777+1DanielSC@users.noreply.github.com>
2021-08-10 10:16:16 -0300

    Create Solution.java

---
+++ 559. Maximum Depth of N-ary Tree/Solution.java
@@ -0,0 +1,36 @@
+
+class Solution {
+  //Solution based on DFS traversal
+      //Time complexity: O(n)
+      //Space Complexity: O(n)
+  
+  
+    public int maxDepth(Node root) {
+        if(root == null)
+            return 0;
+
+        int max = DFS(root,1);
+        
+        return max;
+    }
+    
+    public int DFS(Node root, int currentDepth){
+        if(root == null || root.children.isEmpty())
+            return currentDepth;
+        
+        int maxDepth = 0;
+        currentDepth++;
+        
+        //Since there aren't cycles in a tree, it isn't necessary to use a boolen visited[] array
+        //The result of this solution is the same of BFS, since there aren't edges conecting nodes from the same level
+        for(int i = 0; i < root.children.size();i++){
+            
+            int n = DFS(root.children.get(i), currentDepth);
+            
+            maxDepth = Math.max(maxDepth,n);
+            
+        }
+        
+        return maxDepth;
+    }
+}
