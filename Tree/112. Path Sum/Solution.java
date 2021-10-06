b1d35a3bd7f0498f3833ccbf33e7d192dee995d5
Daniel Sehn Colao <66954777+1DanielSC@users.noreply.github.com>
2021-10-05 22:25:46 -0300

    Create Solution.java

---
+++ Tree/112. Path Sum/Solution.java
@@ -0,0 +1,46 @@
+class Solution {
+  
+    //Time Complexity: O(n)
+    //Space Complexity: O(1)
+  
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null)
+            return false;
+        return traverseSum(root, targetSum,0);
+    }
+    
+    private boolean traverseSum (TreeNode root, int target, int sum){
+        
+        //The parent of this node is not a child node
+        if(root == null)
+            return false;
+
+        
+        if(Solution.isChild(root)){
+            if(target == sum + root.val)
+                return true;
+            else 
+                return false;
+        }
+        else{
+            //The current node isn't a child node
+                //Hence, either root.left or root.right can be null
+            if(traverseSum(root.left, target, sum + root.val))
+                return true;
+            if(traverseSum(root.right, target,sum + root.val))
+                return true;
+        }
+        
+        return false;
+        
+    }
+    
+    private static boolean isChild(TreeNode node){
+        if(node == null)
+            return false;
+        else if(node.left == null && node.right == null)
+            return true;
+        else
+            return false;
+    }
+}
